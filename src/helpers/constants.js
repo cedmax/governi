f613ac1425950e96be79a1e5b87a12a7053f4887
Marco Cedaro <cedmax@users.noreply.github.com>
2021-02-18 10:24:42 +0000

    fix code

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,13 +1,13 @@
-export const ranges = ['top', 'high', 'medium', 'low']
-export const topRange = ranges[0]
+export const ranges = ["top", "high", "medium", "low", "none"];
+export const topRange = ranges[0];
 
 export const roughConfig = {
   options: {
     simplification: 0.1,
-    roughness: 1
-  }
-}
+    roughness: 1,
+  },
+};
 
-export const y = 90
-export const x = 450
-export const angle = 35
+export const y = 90;
+export const x = 450;
+export const angle = 35;
